032e0958cb78f93db83aadba029c93e3efcb7db1
Riyabadoni28 <70255901+Riyabadoni28@users.noreply.github.com>
2020-09-29 01:24:14 -0700

    Add files via upload

---
+++ topbutton.js
@@ -0,0 +1,23 @@
+
+// top button- script strat-////
+ 
+  //Get the button
+  var mybutton = document.getElementById("topBtn");
+  
+  // When the user scrolls down 50px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+//-----  top button- script-end--//
